5924d80e5aefb272e005b3f2826d2978ec14ee2e
Jeremy Eissler <jeissler0@gmail.com>
2025-08-03 11:28:53 -0500

    break out simple button comp for reuse

---
+++ src/components/ControlButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button
+    @click="$emit('click')"
+    :disabled="disabled"
+    :class="[
+      'font-bold rounded-lg transition px-6 py-3 flex items-center gap-2 cursor-pointer',
+      $attrs.class ? '' : defaultClasses,
+      disabled ? 'disabled:bg-gray-400 disabled:cursor-not-allowed' : '',
+    ]"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  disabled?: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+  disabled: false,
+})
+
+defineEmits<{
+  click: []
+}>()
+
+const defaultClasses = 'bg-white text-gray-900 border border-gray-300 hover:bg-gray-50'
+</script>
